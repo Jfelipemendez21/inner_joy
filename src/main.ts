6da9668d58cfaae7843c3dafdc20708dda739f9a
Jose Manuel Sanchez Osma <josmsanchez11@misena.edu.co>
2024-04-18 09:18:34 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: true, // Permitir solicitudes de cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Permitir todos los métodos HTTP
+    allowedHeaders: '*', // Permitir todos los encabezados
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
